de185b478bef041a8b16fd4372faf26f6f0d2a48
Luca Ciriaco <luca.ciriaco@7r1ck.com>
2024-01-27 11:18:52 -0300

    Added initial classed and updated gitignore files

---
+++ Global Game Jam 2024/Assets/Scripts/GameDirector.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDirector : MonoBehaviour
+{
+    public static GameDirector Instance;
+
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
